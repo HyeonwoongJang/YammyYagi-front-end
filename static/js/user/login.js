ce6e979d44653b2bc103274f1c1eea849b8b23bf
hjzheld <dusen7774@naver.com>
2023-11-14 00:55:21 +0900

    Feat: 로그인 기능 추가

---
+++ static/js/user/login.js
@@ -0,0 +1,45 @@
+const frontend_base_url = "http://127.0.0.1:5501";
+const backend_base_url = "http://127.0.0.1:8000";
+
+console.log("연결됨");
+
+async function handleLogin() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const response = await fetch(`${backend_base_url}/user/login/`, {
+    headers: {
+      "content-type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  });
+  if (response.status == 200) {
+    const response_json = await response.json();
+
+    localStorage.setItem("access", response_json.access);
+    localStorage.setItem("refresh", response_json.refresh);
+
+    console.log(response_json);
+
+    const base64Url = response_json.access.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+
+    localStorage.setItem("payload", jsonPayload);
+    alert("환영합니다.");
+    window.location.replace(`${frontend_base_url}/story/`);
+  } else {
+    alert("회원정보가 일치하지 않습니다.");
+  }
+}
