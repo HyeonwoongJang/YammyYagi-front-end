a29d8179a1ca2997c674c19f78339a783dc3e143
myeonghee2 <mang1670@gmail.com>
2023-11-14 22:07:00 +0900

    Feat.QnA작성 기능 추가

---
+++ static/js/user/qna.js
@@ -0,0 +1,30 @@
+async function createQnA() {
+    const content = document.getElementById("content").value;
+
+    try {
+        const response = await fetch(`${backend_base_url}/user/qna/`, {
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("access"),
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ content: content })
+        });
+
+        const response_json = await response.json()
+        const status = response_json["status"]
+
+        if (status == "201" && response.status == 201) {
+            alert(`${response_json["success"]}`)
+            window.location.replace(`${frontend_base_url}/story/index.html`)
+            // console.log('Response received:', response_json);
+
+            return;
+        } else if (status == "400" && response.status == 400) {
+            alert(`${response_json["error"]}`)
+            return;
+        }
+    } catch (error) {
+        console.error("error", error)
+    }
+}
