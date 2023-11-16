0186db1b194e76984d96499b347933ca28f4c022
igsh97 <igsh97@naver.com>
2023-11-16 12:11:40 +0900

    Feat: goto_storydetail_after_createstory

--- static/js/story/create_story.js
+++ static/js/story/create_story.js
@@ -167,8 +167,11 @@ async function createStory(){
     try {
         const response = await fetch(`${backend_base_url}/story/`, options);
         const res_json = await response.json();
+        const id=res_json.story_id
         if(res_json.status==201){
+            console.log(id)
             window.alert(res_json.success)
+            window.location.href = `${frontend_base_url}/story/detail.html?story_id=${id}`;
         }
         else{
             window.alert(res_json.error)
