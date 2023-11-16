42703d62db82ffdbbe0d29be280476876e846e41
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-11-16 11:37:18 +0900

    Design: 댓글 생성 삭제 시 댓글 창을 로드하는 함수와 별개로 토글 식으로 작동하는 함수 추가

--- static/js/story/detail.js
+++ static/js/story/detail.js
@@ -14,7 +14,6 @@ let headers = {
 // 창이 로드될 때의 이벤트 핸들러
 window.onload = () => {
     renderPage();       // 페이지의 주요 내용을 렌더링
-    loadComments();     // 댓글을 로드하고 표시
 }
 
 // 페이지의 주요 내용을 렌더링하는 비동기 함수
@@ -103,9 +102,78 @@ async function renderPage() {
     }
 }
 
+// 댓글창을 열고 닫는 비동기 함수
+async function toggleComments() {
+
+    // 댓글 창의 현재 표시 상태를 확인하고, 토글하여 보이게 하거나 숨김
+    const comment_box = document.getElementById("comment")
+    comment_box.style.display = (comment_box.style.display === 'none' || comment_box.style.display === '') ? 'block' : 'none'
+    
+    try {
+        const story_id = storyIdSearch()    // 현재 스토리의 ID를 가져옴
+        const response = await fetch(`${backend_base_url}/story/${story_id}/comment/`, {
+            method : "GET"
+        })
+
+        const response_json = await response.json()
+
+        const comment_data = response_json["comments"]
+
+        const comment_list = document.getElementById("comment-list")
+        comment_list.innerHTML = "";
+
+        // 각 댓글을 렌더링
+        comment_data.forEach(comment => {
+            const single_comment = document.createElement("div")
+            const author_info = document.createElement("div")
+            const author_profile = document.createElement("img")
+            const author_nickname = document.createElement("p")
+            const delete_button = document.createElement("button")
+            const content = document.createElement("p")
+
+            single_comment.classList.add("single_comment")
+
+            author_info.classList.add("comment_author_info")
+
+            author_profile.src = `${backend_base_url}${comment["author_profile"]}`
+            author_profile.classList.add("comment_author_profile")
+            author_nickname.innerText = comment["author_nickname"]
+
+            delete_button.classList.add("btn-close")
+            delete_button.onclick = function() {
+                deleteComment(comment["comment_id"]);
+            };
+            
+            content.innerText = comment["content"]
+            content.classList.add("comment_content")
+
+            author_info.appendChild(author_profile)
+            author_info.appendChild(author_nickname)
+
+             // 현재 사용자가 로그인되어 있고, 댓글 작성자가 현재 사용자인 경우 삭제 버튼 추가
+            if (localStorage.getItem("access")) {
+                const payload = localStorage.getItem("payload")
+                const payload_parse = JSON.parse(payload)
+                const comment_author = payload_parse.nickname
+                if (comment["author_nickname"] == comment_author) {
+                    author_info.appendChild(delete_button)
+                }
+            }
+
+            single_comment.appendChild(author_info)
+            single_comment.appendChild(content)
+            
+            comment_list.appendChild(single_comment)
+        })
+
+    } catch (error) {
+        alert("댓글 로드 실패")
+    }
+}
+
 // 댓글을 로드하는 비동기 함수
 async function loadComments() {
-
+    
     try {
         const story_id = storyIdSearch()    // 현재 스토리의 ID를 가져옴
         const response = await fetch(`${backend_base_url}/story/${story_id}/comment/`, {
@@ -186,7 +254,7 @@ async function deleteComment(comment_id) {
 
         if (status == "204") {
             alert(`${response_json["success"]}`)
-            loadComments(story_id);                                 // 댓글이 삭제되면 업데이트된 댓글 목록을 로드
+            loadComments();                                         // 댓글이 삭제되면 업데이트된 댓글 목록을 로드
             return;
         } else if(status == "401" && response.status == 401) {
             alert(`${response_json["error"]}`)                      // 권한이 없는 경우
