6a50fe6e5aaaffda5201e970fe80486700f4a8bb
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-11-28 23:58:35 +0900

    Remove: social_login.js 파일 삭제 -> 소셜로그인 시 index.html로 이동 = index.js로 코드 옮김

--- static/js/index.js
+++ static/js/index.js
@@ -340,4 +340,119 @@ window.onload = async function () {
   const stories = await getstories();
   const totalPages = stories.page_info.total_pages;
   renderPagination(initialPage, totalPages);
+
+  startSocialLogin()
 };
+
+// 소셜 로그인
+const currentUrl = location.href;
+async function socialLoginAPI(code) {
+  if (currentUrl.includes("state")) {
+    naverLoginApi(code);
+  } else if (currentUrl.includes("scope")) {
+    googleLoginApi(code);
+  } else {
+    kakaoLoginApi(code);
+  }
+}
+
+// 카카오 로그인 데이터 서버로 전송
+async function kakaoLoginApi(code) {
+  try {
+    const response = await fetch(`${backend_base_url}/user/kakao/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: code }),
+    });
+    response_json = await response.json();
+    if (!response.ok) {
+      alert(response_json["error"]);
+      window.location.href = `${frontend_base_url}`;
+      return;
+    }
+    saveToken(response_json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// 네이버 로그인 데이터 서버로 전송
+async function naverLoginApi(code) {
+  try {
+    const response = await fetch(`${backend_base_url}/user/naver/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: code }),
+    });
+    response_json = await response.json();
+    if (!response.ok) {
+      alert(response_json["error"]);
+      window.location.href = `${frontend_base_url}`;
+      return;
+    }
+    saveToken(response_json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// 구글 로그인 데이터 서버로 전송
+async function googleLoginApi(code) {
+  try {
+    const response = await fetch(`${backend_base_url}/user/google/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code: code }),
+    });
+    response_json = await response.json();
+    if (!response.ok) {
+      alert(response_json["error"]);
+      window.location.href = `${frontend_base_url}`;
+      return;
+    }
+    saveToken(response_json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// access, refresh, payload localstorage에 저장
+async function saveToken(response_json) {
+  localStorage.setItem("access", response_json.access);
+  localStorage.setItem("refresh", response_json.refresh);
+  const base64Url = response_json.access.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+  localStorage.setItem("payload", jsonPayload);
+  window.location.href = `${frontend_base_url}`;
+}
+
+async function startSocialLogin() {
+  if (location.href.split("=")[1]) {
+    // 현재 URL에서 쿼리스트링 파라미터 추출
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    // 로그인을 위한 인가 코드 추출
+    const code = urlParams.get("code");
+
+
+    if (code) {
+      localStorage.setItem("code", code);
+      socialLoginAPI(code);
+    }
+  }
+}
