79154c7d1950f27f001b90e30859b96c9d15f547
igsh97 <igsh97@naver.com>
2023-11-28 12:22:33 +0900

    release: base url 수정 후 다시 배포

--- static/js/user/register.js
+++ static/js/user/register.js
@@ -1,5 +1,6 @@
-const frontend_base_url = "http://127.0.0.1:5501";
-const backend_base_url = "http://127.0.0.1:8000";
+const frontend_base_url = "https://www.yummyyagi.com";
+const backend_base_url = "https://api.yummyyagi.com";
+
 
 // 로그인 체크
 function checkLogin() {
