2e1c0979d52be574ded7858a144b5c3ab0d2fe42
igsh97 <igsh97@naver.com>
2023-11-29 10:11:44 +0900

    release: 4번째 배포

--- static/js/user/login.js
+++ static/js/user/login.js
@@ -1,5 +1,5 @@
-const frontend_base_url = "http://127.0.0.1:5501";
-const backend_base_url = "http://localhost";
+const frontend_base_url = "https://www.yummyyagi.com";
+const backend_base_url = "https://api.yummyyagi.com";
 
 // 로그인 체크
 function checkLogin() {
